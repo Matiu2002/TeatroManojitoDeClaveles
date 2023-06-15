eb7aae9bc02c32f29481f83e5f25a00c1af495d3
Matiu2002 <130482466+Matiu2002@users.noreply.github.com>
2023-06-15 16:32:23 -0400

    Clases sin terminar (prueba)

--- TeatroManojitoDeClaveles/Clases/Evento_FB.cs
+++ TeatroManojitoDeClaveles/Clases/Evento_FB.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeatroManojitoDeClaveles.Enum;
 
 namespace TeatroManojitoDeClaveles.Clases
 {
@@ -10,5 +11,53 @@ namespace TeatroManojitoDeClaveles.Clases
     {
         private List<Colaborador> colaboradores;
         private List<string> instituciones_invitadas;
+        public Evento_FB()
+        {
+            id = 0;
+            fecha = DateTime.Today;
+            horario = "Sin definir";
+            numero_participantes = 50;
+            tipo = new ETipoActividad();
+            costo_actividad = 50000;
+            capacidad = 10000;
+            artistas = new List<Artista>();
+            tipoE = new ETipoEvento();
+            entradas_disponibles = 10000;
+            valores = new ValoresTickets();
+            colaboradores = new List<Colaborador>();
+            instituciones_invitadas = new List<string>();
+        }
+        public Evento_FB(int id, string f, string h, int n, string t, int c, int ca, string te, int ed)
+        {
+            this.id = id;
+            fecha = DateTime.Parse(f);
+            horario = h;
+            numero_participantes = n;
+            //tipo revisar;
+            costo_actividad = c;
+            capacidad = ca;
+            artistas = new List<Artista>();
+            //tipoE revisar;
+            entradas_disponibles = ed;
+            valores = new ValoresTickets();
+            colaboradores = new List<Colaborador>();
+            instituciones_invitadas = new List<string>();
+        }
+        public Evento_FB(Evento_FB e)
+        {
+            id = e.id;
+            fecha = e.fecha;
+            horario = e.horario;
+            numero_participantes = e.numero_participantes;
+            tipo = e.tipo;
+            costo_actividad = e.costo_actividad;
+            capacidad = e.capacidad;
+            artistas = e.artistas;
+            tipoE = e.tipoE;
+            entradas_disponibles = e.entradas_disponibles;
+            valores = e.valores;
+            colaboradores = new List<Colaborador>();
+            instituciones_invitadas = new List<string>();
+        }
     }
 }
